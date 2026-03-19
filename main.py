4d1c010a5476ab08e4ca6152be10bef31c9e9279
andreaserpa <acserpa@miuandes.cl>
2026-03-19 17:54:05 -0300

    funcion lista para leer tareas.txt

--- main.py
+++ main.py
@@ -1,1 +1,12 @@
 def leer_tareas():
+    tareas = []
+    with open("tareas.txt", "r", encoding="utf-8") as f:
+        for linea in f:
+            partes = linea.strip().split(",")
+            tarea = {
+                "id":partes[0],
+                "duracion": int(partes[1]),
+                "categoria": partes[2]
+            }
+            tareas.append(tarea)
+    return tareas
